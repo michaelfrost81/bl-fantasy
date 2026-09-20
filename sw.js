490e50838862ebadf7a6cc761b7ddba2c40f1ca4
michaelfrost81 <michael.frost.81@gmail.com>
2026-09-20 22:05:41 +0200

    Retire service worker and clear stale PWA caches

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
-const CACHE="bl-fantasy-v14";const ASSETS=["./icon-192.png?v=20260920-14","./","./index.html","./styles.css?v=20260920-11","./app.js?v=20260920-11","./manifest.json?v=20260920-14","./apple-touch-icon.png"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{let copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))))});
+self.addEventListener("install",e=>self.skipWaiting());
+self.addEventListener("activate",e=>e.waitUntil(
+  Promise.all([
+    self.registration.unregister(),
+    caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))),
+    self.clients.claim()
+  ])
+));
